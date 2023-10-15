73b4deb11b93747becc65281b4687683e68f4b82
DelfinaMolter <delfi.molter@gmail.com>
2023-10-14 23:04:34 -0300

    Primer commit

---
+++ app/layout.js
@@ -0,0 +1,17 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Beer Club',
+  description: 'En Beer Club vas a encontrar a los gururs que te guiarán para tener la mejores experiencia degutando cervezas de todo el mundo.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
