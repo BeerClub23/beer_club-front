f56d18fd5e059271acf760e0a9b555b38eeb3b80
lalopez13 <lorojas.laure@gmail.com>
2023-10-23 15:47:08 -0500

    add styles, modifying links

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -1,3 +1,4 @@
+import "./footer.scss";
 import { Box, Stack, Container, Grid, Typography } from "@mui/material";
 import Logo from "../../../public/images/logo/Logo_sin_escudo_Negro.svg";
 import FacebookIcon from "@mui/icons-material/Facebook";
@@ -18,35 +19,56 @@ export default function Footer() {
         paddingTop: "2rem",
         paddingBottom: "1rem",
         color: "#000000",
+        bottom: "0",
       }}
     >
       <Container maxWidth="lg">
-        <Grid container spacing={2} alignItems="center" columnSpacing={{ sm: 1 }}>
+        <Grid
+          container
+          alignItems="center"
+          justifyContent={"center"}
+          spacing={2}
+        >
           <Grid item xs={12} sm={4}>
-            <Link href={"../../home"}>
-              <Image src={Logo} width={150} height={150} alt={'logo Beer Club'}/>
+            <Link href={"/home"}>
+              <Image
+                src={Logo}
+                width={130}
+                height={130}
+                alt={"logo Beer Club"}
+              />
             </Link>
           </Grid>
-          <Grid item xs={12} sm={4}  >
-            <Typography variant="body1" component="div" fontWeight="bold">
-              <Link href="https://example.com">Preguntas frecuentes</Link>
-            </Typography>
-            <Typography variant="body1" component="div" fontWeight="bold">
-              <Link href="https://example.com">Información legal</Link>
-            </Typography>
-            <Typography variant="body1" component="div" fontWeight="bold">
-              <Link href="https://example.com">Acerca de nosotros</Link>
-            </Typography>
-            <Typography variant="body1" component="div" fontWeight="bold">
-              <Link href="https://example.com">Contáctanos</Link>
-            </Typography>
+          <Grid item xs={12} sm={4} className="linkContainer">
+            <div>
+              <Typography variant="body1" component="div" className="linkItem" sx={{fontWeight:"bold",marginBottom:"2px"}}>
+                <Link href="/faqs">Preguntas frecuentes</Link>
+              </Typography>
+              <Typography variant="body1" component="div" className="linkItem" sx={{fontWeight:"bold",marginBottom:"2px"}}>
+                <Link href="/informacion-legal">Información legal</Link>
+              </Typography>
+              <Typography variant="body1" component="div" className="linkItem" sx={{fontWeight:"bold",marginBottom:"2px"}}>
+                <Link href="/nosotros">Acerca de nosotros</Link>
+              </Typography>
+              <Typography variant="body1" component="div" className="linkItem" sx={{fontWeight:"bold",marginBottom:"2px"}}>
+                <Link href={`https://wa.me/${process.env.PHONE_NUMBER}?text=Bienvenido+a+beer+club+%21%21%21`}>Contáctanos</Link>
+              </Typography>
+            </div>
           </Grid>
-          <Grid item xs={12} sm={4} sx={{ textAlign: "center" }}>
-            <Stack direction="row" alignItems="center" spacing={3}>
-              <FacebookIcon sx={{ fontSize: "30px" }} />
-              <YouTubeIcon sx={{ fontSize: "30px" }} />
-              <TwitterIcon sx={{ fontSize: "30px" }} />
-              <InstagramIcon sx={{ fontSize: "30px" }} />
+          <Grid item xs={12} sm={4}>
+            <Stack direction="row" spacing={3} className="socialStack">
+              <Link href="https://www.facebook.com">
+                <FacebookIcon className="socialIcon" sx={{fontSize:"30px"}} />
+              </Link>
+              <Link href="https://www.youtube.com">
+                <YouTubeIcon className="socialIcon" sx={{fontSize:"30px"}} />
+              </Link>
+              <Link href="https://www.twitter.com">
+                <TwitterIcon className="socialIcon" sx={{fontSize:"30px"}}/>
+              </Link>
+              <Link href="https://www.instagram.com">
+                <InstagramIcon className="socialIcon" sx={{fontSize:"30px"}}/>
+              </Link>
             </Stack>
           </Grid>
         </Grid>
