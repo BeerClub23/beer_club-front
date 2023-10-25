aa0b0f1534f2de1f851f35755d7775009273d3e0
Clara <lisleclara1@gmail.com>
2023-10-25 10:48:00 -0300

    styles about us

--- app/components/aboutUsCard/AboutUsCard.jsx
+++ app/components/aboutUsCard/AboutUsCard.jsx
@@ -7,13 +7,13 @@ import lupulo from '../../../public/images/aboutUs/lupulo.png'
 export const AboutUsCard = () => {
   return (
       <article  className={styles.containerArticle}>      
-          <Image src={chop} width={120} height={120} alt='logo' className={styles.containerArticle_logo1} />   
-          <Image src={chop} width={120} height={120} alt='logo' className={styles.containerArticle_logo2} />   
-            <div className={styles.containerArticle_textContainer}  data-aos="fade-up">
-                <p className={styles.containerArticle_text}>¡Imaginate un mundo en el que cada sorbo es como una nueva aventura! Donde cada cerveza es como abrir la puerta a un mundo lleno de sabores, olores y tradiciones por descubrir. Cuando te sumás a Beer Club, te lanzás de cabeza a un lugar lleno de descubrimientos y buenos momentos. </p>
-                <p className={styles.containerArticle_text}>Todos los meses, recibirás en tu puerta una cuidadosa selección de cervezas artesanales de todo el mundo. Además, creamos experiencias únicas con eventos exclusivos, catas virtuales protagonizadas por cerveceros de renombre y te conectamos con otros amantes apasionados de la cerveza... </p>
-                <p className={styles.containerArticle_text}>Si estás listo para unirte a una movida que celebra la diversidad de la cerveza y para experimentar un mundo de sabores y camaradería que solo los verdaderos fanáticos de la birra pueden disfrutar, entonces Beer Club es tu destino cervecero definitivo.</p>
-                <p className={styles.containerArticle_spanText}><span >... y salud a la pasión por la cerveza!</span></p>
+          <Image src={chop} width={100} height={142} alt='logo' className={styles.containerArticle_logo1} />   
+          <Image src={chop} width={100} height={142} alt='logo' className={styles.containerArticle_logo2} />   
+            <div className={styles.containerArticle_textContainer}  >
+                <p className={styles.containerArticle_text}data-aos="fade-up">¡Imaginate un mundo en el que cada sorbo es como una nueva aventura! Donde cada cerveza es como abrir la puerta a un mundo lleno de sabores, olores y tradiciones por descubrir. Cuando te sumás a Beer Club, te lanzás de cabeza a un lugar lleno de descubrimientos y buenos momentos. </p>
+                <p className={styles.containerArticle_text}data-aos="fade-up" >Todos los meses, recibirás en tu puerta una cuidadosa selección de cervezas artesanales de todo el mundo. Además, creamos experiencias únicas con eventos exclusivos, catas virtuales protagonizadas por cerveceros de renombre y te conectamos con otros amantes apasionados de la cerveza... </p>
+                <p className={styles.containerArticle_text}data-aos="fade-up">Si estás listo para unirte a una movida que celebra la diversidad de la cerveza y para experimentar un mundo de sabores y camaradería que solo los verdaderos fanáticos de la birra pueden disfrutar, entonces Beer Club es tu destino cervecero definitivo.</p>
+                <p className={styles.containerArticle_spanText}data-aos="fade-up"><span >... y salud a la pasión por la cerveza!</span></p>
             </div>
             <div className={styles.containerArticle_imageAside}> 
                 <Image src={lupulo} width={220} height={285} className={styles.containerArticle_pic}alt='logo' />   
