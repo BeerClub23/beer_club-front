0654a545b536c639e8b3bd774a5066a3f91df5a4
Clara <lisleclara1@gmail.com>
2023-10-30 13:08:23 -0300

    styles

--- app/home/about/Section.jsx
+++ app/home/about/Section.jsx
@@ -4,41 +4,43 @@ import { useGetAboutSteps } from '../../services/about';
 import { Container } from '@mui/material';
 import AboutStepCard from '../../components/aboutCard/AboutCard'
 import styles from './AboutSection.module.scss'
+import {aboutSteps} from '../../services/about'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 4,
-  slidesToScroll: 0,
-  initialSlide: 0,
-  arrows: true,
-  responsive: [    
-    {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        initialSlide: 0
-      }
-    },
-    {
-      breakpoint: 900,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
-};
+
 
 const AboutSection = () => {
   // const { data, isLoading, isError} = useGetAboutSteps();
-  
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 0,
+    initialSlide: 0,
+    arrows: true,
+    responsive: [    
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 0
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
  
+
   return (
     <section id='como-funciona' className={styles.aboutSection}>
           <h2 className={styles.aboutSection_title }>
@@ -47,12 +49,12 @@ const AboutSection = () => {
         <Container>        
           <article className={styles.aboutSection_aboutArticle}> 
           <Slider {...settings}>
-            {data.map((step, index)=>         
+            {aboutSteps.map((step, index)=>         
             <AboutStepCard
               key={index}
               data={step}
               />)}
-          </Slider> 
+          </Slider>  
           </article>    
 
       </Container>
