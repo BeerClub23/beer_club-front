7018bc5e45e3740616593d356155f37ec6b6481d
lalopez13 <lorojas.laure@gmail.com>
2023-12-03 23:41:14 -0500

    add deactivate account button for user

--- app/user/layout.jsx
+++ app/user/layout.jsx
@@ -3,7 +3,7 @@ import "../globals.scss";
 import React, { useEffect } from "react";
 import Footer from "../components/footer/Footer";
 import HeaderPublic from "../components/headerPublic/headerPublic";
-import { getUserInfo } from "../services/user";
+import { deleteUser, getUserInfo } from "../services/user";
 import Cookies from "js-cookie";
 import UserSideBar from "../components/userSideBar/UserSideBar";
 import UserData from "../components/userBasicData/UserBasicData";
@@ -13,6 +13,8 @@ import { jwtDecode } from "jwt-decode";
 import { usePathname } from "next/navigation";
 import { memberItems } from "../common/constants/NavBarItems";
 import { useGetSubscriptions } from "../services/subscriptions";
+import { useRouter } from "next/navigation";
+import Swal from "sweetalert2";
 
 const UserLayout = ({ children }) => {
   const pathname = usePathname();
@@ -21,6 +23,7 @@ const UserLayout = ({ children }) => {
 
   const token = Cookies.get("jwt");
   const decodeToken = token ? jwtDecode(token.toString()) : null;
+  const router = useRouter();
 
   useEffect(() => {
     const userInfo = async () => {
@@ -37,12 +40,100 @@ const UserLayout = ({ children }) => {
             (subscription) => subscription.id === response.nextSubscriptionId,
           );
         }
-        setUser({ ...response, subscription: currentSubscription, nextSubscription});
-        console.log(user)
+        setUser({
+          ...response,
+          subscription: currentSubscription,
+          nextSubscription,
+        });
+        console.log(user);
       });
     }
   }, [subscriptions]);
 
+  // Handle deactivate user account
+  const handleDeactivateAccount = async () => {
+    let timerInterval;
+    Swal.fire({
+      title: "¿Estás seguro?",
+      text: "Esta acción no se puede revertir.",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#A9A9A9",
+      cancelButtonColor: "#ceb5a7",
+      confirmButtonText: "Confirmar",
+      cancelButtonText: "Cancelar",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          console.log("userId: " + user.id);
+          let response = await deleteUser(user.id, token);
+
+          if (response.status === 204) {
+            Swal.fire({
+              title: "Cuenta desactivada",
+              text: "Tu cuenta ha sido desactivada exitosamente.",
+              icon: "success",
+              confirmButtonText: "OK",
+              confirmButtonColor: "#ceb5a7",
+            }).then((result) => {
+              if (result.isConfirmed) {
+                Swal.fire({
+                  title: "¡Gracias por haber sido parte de Beer Club!",
+                  timer: 2000,
+                  showConfirmButton: false,
+                  didOpen: () => {
+                    timerInterval = setInterval(() => {
+                      const timer = Swal.getPopup().querySelector("b");
+                      if (timer) {
+                        timer.textContent = `${Swal.getTimerLeft()}`;
+                      }
+                    }, 100);
+                  },
+                  willClose: () => {
+                    clearInterval(timerInterval);
+                  },
+                }).then((result) => {
+                  if (result.dismiss === Swal.DismissReason.timer) {
+                    console.log("I was closed by the timer");
+                  }
+                });
+                // Logout logic - add delay
+                setTimeout(() => {
+                  Cookies.remove("jwt");
+                  setUser(null);
+                  router.push("/home");
+                }, 2000);
+              }
+            });
+          } else {
+            // Handle other response statuses (e.g., 404, 500) or error responses
+            const error = Object.keys(response.response.data).reduce(
+              (acc, key) => `${acc}${response.response.data[key]}\n`,
+              "",
+            );
+            Swal.fire({
+              title: "Error!",
+              text: error || "Unexpected error occurred.",
+              icon: "error",
+              confirmButtonText: "Continuar",
+              confirmButtonColor: "#ceb5a7",
+              focusConfirm: false,
+            });
+          }
+        } catch (error) {
+          // Handle errors that occur during the request (e.g., network error)
+          Swal.fire({
+            title: "Error!",
+            text: "Unexpected error occurred.",
+            icon: "error",
+            confirmButtonText: "Continuar",
+            confirmButtonColor: "#ceb5a7",
+            focusConfirm: false,
+          });
+        }
+      }
+    });
+  };
   return (
     <div
       style={{
@@ -63,7 +154,7 @@ const UserLayout = ({ children }) => {
           }}
           id="cont"
         >
-          <UserSideBar />
+          <UserSideBar handleDeactivateAccount={handleDeactivateAccount} />
           <Box sx={{ padding: "30px" }} id="user_wrapper">
             {user ? (
               <>
